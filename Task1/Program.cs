2c3401355fa2f444778d9fe987c5bffd2a34fdb5
«Anisa» <an1sa_jon@mail.ru>
2022-10-24 18:27:03 +0300

    Создаем метод заполнения нового массива

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -17,7 +17,7 @@ return el;
 
 string[] mas1 = {"fdf", "wert", "1", "4567"};
 System.Console.WriteLine(KolEllements(mas1)); 
-return else;
+return el;
 }
 string[] Mas2(int el)
 {
@@ -25,5 +25,33 @@ string[] array2 = new string [el];
 return array2;
 }
 
+string[] MasNew(string[] array, string[] array2)
+{
+int length = array.Length;
+int lengthString = 3;
+int i =0; int j =0;
+    while (i<length)
+    {
+        if (array[i].Length <= lengthString)
+        {
+            array2[j] = array[i]; i++; j++;   
+        } 
+        else i++;   
+    }
+return array2;
+}
+
+void PrintMas(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
 string[] mas1 = {"fdf", "wert", "1", "4567"};
-System.Console.WriteLine(KolEllements(mas1)); 
+Console.WriteLine("ИСХОДНЫЙ МАССИВ");
+PrintMas(mas1); 
+//Console.WriteLine("Кол-во эл-тов состоящих из менее 3х символов =" + KolEllements(mas1)); 
+Console.WriteLine("НОВЫЙ МАССИВ");
+PrintMas(MasNew(mas1, Mas2(KolEllements(mas1))));
